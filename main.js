56190b723508a2357aa8bf8407f5175bd3f4f563
spraylee <spraylee@qq.com>
2020-04-13 11:30:15 +0800

    add time

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ app.post('/removebg', async (req, res) => {
       return res.status(400).send({ reason: `no url provided in body` })
     }
     const result = await getImage(req.body.url)
-    res.send({ result })
+    res.send(result)
   } catch (e) {
     res.status(500).send({ reason: e.message })
   }
@@ -31,16 +31,39 @@ app.post('/removebg', async (req, res) => {
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 async function getImage(url) {
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-  const page = await browser.newPage()
-  page.on('dialog', async (dialog) => {
-    await dialog.accept(url)
-  })
-  await page.goto('https://www.remove.bg/upload')
-  const btn = await page.waitForSelector('.select-photo-url-btn')
-  btn.click()
-  await page.waitForSelector('img.transparency-grid')
-  const resultImageSrc = await page.$eval('img.transparency-grid', (el) => el.src)
-  await browser.close()
-  return resultImageSrc
+  let browser = null
+  try {
+    const start = new Date().getTime()
+    let beforeInputUrl = 0
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+    const afterBrowser = new Date().getTime()
+    const page = await browser.newPage()
+    const afterPage = new Date().getTime()
+    page.on('dialog', async (dialog) => {
+      beforeInputUrl = new Date().getTime()
+      await dialog.accept(url)
+    })
+    await page.goto('https://www.remove.bg/upload')
+    const btn = await page.waitForSelector('.select-photo-url-btn')
+    const afterPageOpen = new Date().getTime()
+    btn.click()
+    await page.waitForSelector('img.transparency-grid')
+    const resultImageSrc = await page.$eval('img.transparency-grid', (el) => el.src)
+    const afterResult = new Date().getTime()
+    await browser.close()
+    return {
+      time: {
+        openBrowser: afterBrowser - start,
+        openPage: afterPage - afterBrowser,
+        openSite: afterPageOpen - afterPage,
+        inputImageUrl: beforeInputUrl - afterPageOpen,
+        getResult: afterResult - beforeInputUrl,
+        total: afterResult - start,
+      },
+      result: resultImageSrc,
+    }
+  } catch (e) {
+    browser && browser.close()
+    throw e
+  }
 }
