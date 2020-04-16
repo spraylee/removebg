aca5dbfa73282c6dcf736e6993d68dbb190ef6d8
spraylee <spraylee@qq.com>
2020-04-16 11:54:15 +0800

    update wait time: 30s -> 60s

--- main.js
+++ main.js
@@ -56,14 +56,14 @@ async function getImage(url, proxy, id) {
       console.log(`${id}: input url`)
       await dialog.accept(url)
     })
-    await page.goto('https://www.remove.bg/upload')
+    await page.goto('https://www.remove.bg/upload', { timeout: 60 * 1000 })
     const btn = await page.waitForSelector('.select-photo-url-btn')
     const afterPageOpen = new Date().getTime()
     console.log(`${id}: site is open`)
     btn.click()
     await Promise.race([
       new Promise(async (resolve, reject) => {
-        resolve(await page.waitForSelector('img.transparency-grid').catch((err) => reject(err)))
+        resolve(await page.waitForSelector('img.transparency-grid', { timeout: 60 * 1000 }).catch((err) => reject(err)))
       }),
       new Promise(async (resolve, reject) => {
         await page.waitForSelector('.checkbox-captcha').catch((err) => reject(err))
